3757c6c0911c704d521fbb8b53c7feb89cc68225
Raul Sanchez R <raul.sr92@gmail.com>
2023-05-08 12:00:11 -0500

    Avance clase 2

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
-console.log(maps)
+/* 1° Capturar al elemento canvas de html*/
+
+const canvas = document.querySelector("#game");
+console.log(canvas);
+
+/* 2° Crear el contexto de Canvas */
+
+const game = canvas.getContext("2d");
+
+/*3°Crear evento y función que va a contener las propiedades y métodos del contexto 2D */
+
+window.addEventListener("load", startGame);
+
+
+function startGame(){
+
+}
