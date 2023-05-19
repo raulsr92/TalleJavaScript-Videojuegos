8d9bca5ed2cf44aa57a42ba0ac9a4e2acbb0bb29
Raul Sanchez R <raul.sr92@gmail.com>
2023-05-19 17:47:16 -0500

    Avance de la clase 3 - Tamaño del canvas y sus elementos

--- game.js
+++ game.js
@@ -6,24 +6,45 @@ console.log(canvas);
 /* 2° Crear el contexto de Canvas */
 
 const game = canvas.getContext("2d");
+console.log(game);
+
 
 /*3°Crear evento y función que va a contener las propiedades y métodos del contexto 2D */
 
 window.addEventListener("load", startGame);
 
+
 function startGame(){
+
+    let canvaSize;
+
+    if(window.innerHeight > window.innerWidth){
+        canvaSize = window.innerWidth *0.8;
+    } else{
+        canvaSize= window.innerHeight * 0.8
+    }
+
+
+    canvas.setAttribute("height", canvaSize);
+    canvas.setAttribute("width", canvaSize);
+
+    console.log(window.innerHeight);
+    console.log(window.innerWidth);
+
+    console.log(canvas.height);
+    console.log(canvas.width);
+
+    /*
     game.fillRect(5, 5, 100, 100);
     console.log(game);
-
     game.clearRect(5, 5, 50, 50);
-
     game.clearRect(55, 55, 50, 50);
-
     game.font="20px Verdana";
     game.fillStyle = "Green";
     game.textAlign = "right";
-    game.fillText("Raúl", 50, 35);
+    game.fillText("Raúl", 50, 35);*/
 
+    
 
 
 };
